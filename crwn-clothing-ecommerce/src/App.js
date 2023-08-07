09809055bdac4991ad6725552af687e87cb7a99e
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-08-07 15:34:30 +0800

    Refactor directory component

--- crwn-clothing-ecommerce/src/App.js
+++ crwn-clothing-ecommerce/src/App.js
@@ -1,6 +1,6 @@
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
 
-import './categories.styles.scss';
+import './directory.styles.scss';
 
 const App = () => {
   const categories = [
@@ -31,13 +31,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
