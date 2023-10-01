22b979a99354595f97f27b6e1b71e41989a01ae5
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-10-02 03:38:46 +0800

    Create category page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,16 @@
-import { Fragment, useContext } from 'react';
+import { Routes, Route } from 'react';
 
-import ProductCard from '../../components/product-card/product-card.component';
-
-import { CategoriesContext } from '../../contexts/categories.context';
 import CategoryPreview from '../../components/category-preview/category-preview.components';
+import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoryPreview />}></Route>
+      <Route path=":category" element={<Category />}></Route>
+    </Routes>
   );
 };
 
