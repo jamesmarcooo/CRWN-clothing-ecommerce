b8b34204237687bad888c02474b5a906fde33871
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-10-13 13:02:48 +0800

    Created redux store, and user reducer and action

--- src/index.js
+++ src/index.js
@@ -6,20 +6,24 @@ import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react';
+import { store } from './store/store';
 
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
