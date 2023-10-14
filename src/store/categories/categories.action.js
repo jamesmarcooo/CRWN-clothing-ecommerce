3ba079d323b86b4cac68048761dc736f3e0c4f2b
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-10-14 16:40:08 +0800

    Create a memoized selector for categories

--- src/store/categories/categories.action.js
+++
@@ -1,5 +0,0 @@
-import CATEGORIES_ACTION_TYPES from './categories.types';
-import { createAction } from '../../utils/reducer/reducer.utils';
-
-export const setCategories = (categoriesMapArray) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesMapArray);
